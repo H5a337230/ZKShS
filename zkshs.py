420a1b473c89d927e65feea30edc3c5599aa85f1
H5a337230 <H5a337230@users.noreply.github.com>
2019-06-18 20:56:01 +0000

    add alert functions, dns, reverse_dns, honey_score

--- zkshs.py
+++ zkshs.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import sys
-reload(sys)  
-sys.setdefaultencoding('utf-8')
+#reload(sys)  
+#sys.setdefaultencoding('utf-8')
 import requests.packages.urllib3
 from json import dumps, loads
 import requests
@@ -14,10 +15,10 @@ from optparse import OptionGroup
 from kr import kre
 from ficls import fcls
 
-Tversion = 'VERSION 0.1'
+Tversion = 'VERSION 0.9'
 kc = kre()
 fc = fcls()
-requests.packages.urllib3.disable_warnings()
+#requests.packages.urllib3.disable_warnings()
 
 #################################
 
@@ -86,6 +87,7 @@ def manFunc(cfil):
 		if (cfil == fc.CAM_TAGs[fnum].lower()):
 			print (Fore.YELLOW+fc.CAM_TAGs_COM[fnum]+'\n'+Style.RESET_ALL)
 			sys.exit()
+	print (Fore.RED+'[!] Wrong filter name.'+Style.RESET_ALL)
 
 #################################
 
@@ -258,6 +260,200 @@ def shmainReq(Squery,Skey,limitQ,pageQ):
 
 #################################
 
+
+def Hchecker(tip):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.get('https://api.shodan.io/labs/honeyscore/'+tip+'?key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			print (Fore.GREEN+str(response)+Style.RESET_ALL)
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def dnscheck(hname):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.get('https://api.shodan.io/dns/resolve?hostnames='+hname+'&key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			for item in hname.split(','):
+				print (Fore.GREEN+item+': '+str(response[item])+Style.RESET_ALL)
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def Rdnscheck(rev_ip):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.get('https://api.shodan.io/dns/reverse?ips='+rev_ip+'&key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			for item in rev_ip.split(','):
+				if (not response[item]):
+					print (Fore.YELLOW+'None for '+item+Style.RESET_ALL)
+				else:
+					print (Fore.GREEN+item+': '+str(response[item][0])+Style.RESET_ALL)
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def newalert(name,target,expire_time):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	if (not expire_time):
+		json_data = {'name': name,'filters': {'ip': target}, 'key': api_key}
+	else:
+		json_data = {'name': name,'filters': {'ip': target}, 'expires': expire_time, 'key': api_key}
+	try:
+		response = requests.post('https://api.shodan.io/shodan/alert', data=json_data)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			print (Fore.GREEN+'alert created and '+Style.RESET_ALL)
+		#print (loads(response.text)['error'])
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def alertinfo(alert_id):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.get('https://api.shodan.io/shodan/alert/'+alert_id+'/info?key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			print (Fore.GREEN+'alert name: '+str(response['name'])+Fore.MAGENTA+'    created date: '+str(response['created'])+Fore.BLUE+'    ip: '+str(response['filters']['ip'][0])+Fore.YELLOW+'    id: '+str(response['id']))
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def alertdel(alert_id):
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.delete('https://api.shodan.io/shodan/alert/'+alert_id+'?key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			print (Fore.GREEN+'Your specified alert deleted'+Style.RESET_ALL)
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
+def alertlist():
+	if (kc.ckfile()):
+		if (len(kc.tkeys) == 1):
+			api_key = kc.tkeys[0]
+		else:
+			api_key = kc.chokey()
+	else:
+		print (Fore.RED+'[!] There is something WRONG with Key file(Maybe its EMPTY or Key file NOT EXISTS)'+Style.RESET_ALL)
+	try:
+		response = requests.get('https://api.shodan.io/shodan/alert/info?key='+api_key)
+		if (response.status_code == 401):
+			try:
+				print (Fore.RED+'[!] '+str(response.json()['error'])+Style.RESET_ALL)
+			except Exception as e:
+				print (Fore.RED+'[!] Invalid API key'+Style.RESET_ALL)
+			sys.exit()
+		else:
+			response = loads(response.text)
+			for alertcount in range(len(response)):
+				print (Fore.GREEN+'alert name: '+str(response[alertcount]['name'])+Fore.MAGENTA+'    created date: '+str(response[alertcount]['created'])+Fore.BLUE+'    ip: '+str(response[alertcount]['filters']['ip'][0])+Fore.YELLOW+'    id: '+str(response[alertcount]['id']))
+	except Exception as e:
+		print (Fore.RED+'[!] Failed, Try Again.\t'+str(e)+Style.RESET_ALL)
+
+
+#################################
+
+
 if __name__=='__main__':
 	print (Fore.CYAN + '''
 				 _____        __ _     __    
@@ -278,9 +474,10 @@ if __name__=='__main__':
 		If you want to see info about each filter, use ' --man <filter_name> ' command.
 		There is possibilty to execute custom queries and if you want to search shodan
 		with your own query, you can use ' --cquery <QUERY> ' command.
+		Now, You can use DNS/Reverse_DNS/honeypot checking services and
+		using alert service to monitor a network range.
+		ALERT FUNCTIONS ARE EXPERIMENTAL.
 		<< I extremly suggest to Read description for query that you want to use. >>
-		OUTPUT STRUCTURE:
-		IP ... PORT ... ISP ... COUNTRY ... RETURNED_DATA
 		'''+Style.RESET_ALL)
 	parser = optparse.OptionParser( version = Tversion )
 	group = OptionGroup(parser,'Filter Options')
@@ -293,23 +490,66 @@ if __name__=='__main__':
 	group.add_option('--qlimit', action='store', dest='limitN' , help='will limit the returned data')
 	parser.add_option_group(group)
 	group = OptionGroup(parser,'Api-Key Options')
-	group.add_option('--kf', action='store', dest='keyfunk' , help='Add or Delete Key(s), print API-KEY help menu and also list all KEYs [default is list KEYs - add|del|help|list]' , type='string' , default='list')
+	group.add_option('--kf', action='store', dest='keyfunk' , help='Add or Delete Key(s), print API-KEY help menu and also list all KEYs [default is list KEYs - add|del|help|list]' , type='string')
 	group.add_option('--api', action='store', dest='api_key' , help='API-KEY')
+	parser.add_option_group(group)
+	group = OptionGroup(parser,'HoneyPot Checker')
+	group.add_option('--honeycheck', action='store_true', default=False, dest='honeycheck', help='Calculates honeypot probability score ranging from 0 (not a honeypot) to 1.0 (is a honeypot) of target ip')
+	group.add_option('--hoip', action='store', dest='honey_ip', help='IP address of target to check if it is honeypot or not')
+	parser.add_option_group(group)
+	group = OptionGroup(parser,'DNS/Reverse_DNS')
+	group.add_option('--dhost', action='store', dest='dns_host', help='hostname(s) - separated with comma')
+	group.add_option('--rdnsip', action='store', dest='rdns_IP', help='IP(s) for reverse DNS - separated with comma')
+	parser.add_option_group(group)
+	group = OptionGroup(parser,'Network Alert')
+	group.add_option('--calert', action='store_true', default=False, dest='create_alert', help='create a network alert for a defined IP/netblock which can be used to subscribe to changes/ events that are discovered within that range.')
+	group.add_option('--alertname', action='store', dest='alert_name', help='defind name for created alert')
+	group.add_option('--alerttarget', action='store', dest='alert_target', help='target IP/netblock for alert')
+	group.add_option('--alertexp', action='store', dest='alert_expire', help='Number of seconds that the alert should be active(OPTIONAL)')
+	group.add_option('--alertid', action='store', dest='alert_id', help='alert id to check its info')
+	group.add_option('--alertin', action='store_true', default=False, dest='alert_info')
+	group.add_option('--dalert', action='store', dest='delete_alert', help='hostname(s) - separated with comma')
+	group.add_option('--lalerts', action='store', dest='list_alerts', help='hostname(s) - separated with comma')
 	parser.add_option_group(group)
 	options,_ = parser.parse_args()
 	###
-	if (options.listf and not (options.fname and options.stfilter and options.dyfilter and options.cqu and options.keyfunk and options.api_key)):
+	if (options.listf and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.fname and options.honey_ip and options.rdns_IP and options.stfilter and options.dyfilter and options.cqu and options.keyfunk and options.api_key and options.dns_host and options.honeycheck)):
 		listFunc(options.listf.lower())
 	###
-	elif (options.fname and not (options.listf and options.stfilter and options.dyfilter and options.cqu and options.keyfunk and options.api_key)):
+	elif (options.fname and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.listf and options.honey_ip and options.rdns_IP and options.stfilter and options.dyfilter and options.cqu and options.keyfunk and options.api_key and options.dns_host and options.honeycheck)):
 		manFunc(options.fname.lower())
 	###
-	elif (options.stfilter or options.dyfilter or options.cqu and not (options.keyfunk and options.api_key and options.fname and options.listf)):
+	elif (options.stfilter or options.dyfilter or options.cqu and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.keyfunk and options.honey_ip and options.rdns_IP and options.api_key and options.fname and options.listf and options.dns_host and options.honeycheck)):
 		shprereq(options.stfilter,options.dyfilter,options.cqu)
 	###
-	elif (options.keyfunk and not (options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+	elif (options.keyfunk and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.fname and options.honey_ip and options.stfilter and options.dyfilter and options.rdns_IP and options.cqu and options.listf and options.dns_host and options.honeycheck)):
 		keyF(options.keyfunk.lower(),options.api_key)
+	### az inja
+	elif (options.honeycheck and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.keyfunk and options.fname and options.stfilter and options.rdns_IP and options.dyfilter and options.dns_host and options.cqu and options.listf)):
+		if (options.honey_ip):
+			Hchecker(options.honey_ip)
+		else:
+			print (Fore.RED+'[!] You did not Enter The Target IP Address'+Style.RESET_ALL)
+	###
+	elif (options.dns_host and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.rdns_IP and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		dnscheck(options.dns_host)
+	###
+	elif (options.rdns_IP and not (options.alert_id and options.alert_name and options.alert_target and options.alert_expire and options.create_alert and options.list_alerts and options.delete_alert and options.alert_info and options.dns_host and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		Rdnscheck(options.rdns_IP)
+	###
+	elif (options.create_alert and options.alert_name and options.alert_target and not (options.alert_id and options.list_alerts and options.delete_alert and options.alert_info and options.rdns_IP and options.dns_host and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		newalert(options.alert_name,options.alert_target,options.alert_expire)
+	###
+	elif (options.alert_id and options.alert_info and not (options.create_alert and options.alert_name and options.alert_target and options.list_alerts and options.delete_alert and options.rdns_IP and options.dns_host and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		alertinfo(options.alert_id)
+	###
+	elif (options.alert_id and options.delete_alert and not (options.create_alert and options.alert_name and options.alert_target and options.list_alerts and options.alert_info and options.rdns_IP and options.dns_host and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		alertdel(options.alert_id)
+	###
+	elif (options.list_alerts and not (options.create_alert and options.alert_name and options.alert_target and options.alert_id and options.delete_alert and options.alert_info and options.rdns_IP and options.dns_host and options.honey_ip and options.keyfunk and options.honeycheck and options.fname and options.stfilter and options.dyfilter and options.cqu and options.listf)):
+		alertlist(options.list_alerts)
 	###
 	else:
-		print (Fore.RED+'[!] Please Check Input Arguments'+Style.RESET_ALL)
+		parser.print_help()
+		sys.exit()
 	print(Style.RESET_ALL)
